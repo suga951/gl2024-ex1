221c0fc578d6d28bca19cafdf8a1f43dac5798b2
Agustin Lopez <aalopezb@gmail.com>
2024-05-16 23:50:33 -0300

    localStorage

--- scripts/skins.js
+++ scripts/skins.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const homeButton = document.querySelector(".menu-btn");
   const skinContainer = document.querySelector(".skins-container");
   const firstSkin = document.getElementById("skin-1");
   const secondSkin = document.getElementById("skin-2");
@@ -11,25 +10,33 @@ document.addEventListener("DOMContentLoaded", () => {
   const changeSkin = (num) => {
     let selectedSkin = `../assets/img/zac_${num}.webp`;
     skinContainer.style.backgroundImage = `url("${selectedSkin}")`;
-
+  };
+  const checkIfViewed = (num) => {
+    localStorage.setItem(`viewedSkin-${num}`, "true");
   };
 
   firstSkin.addEventListener("click", () => {
     changeSkin(1);
+    checkIfViewed(1);
   });
   secondSkin.addEventListener("click", () => {
     changeSkin(2);
+    checkIfViewed(2);
   });
   thirdSkin.addEventListener("click", () => {
     changeSkin(3);
+    checkIfViewed(3);
   });
   fourthSkin.addEventListener("click", () => {
     changeSkin(4);
+    checkIfViewed(4);
   });
   fifthSkin.addEventListener("click", () => {
     changeSkin(5);
+    checkIfViewed(5);
   });
   sixthSkin.addEventListener("click", () => {
     changeSkin(6);
+    checkIfViewed(6);
   });
 });
